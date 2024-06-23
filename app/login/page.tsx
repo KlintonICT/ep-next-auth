67351057f7b42200627f7ba5820bfa24d4160c67
KlintonICT <chhunklinton91@gmail.com>
2024-06-23 15:47:38 +0700

    feat(page): create login form and page

---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
+
+import Form from './form';
+
+export default async function LoginPage() {
+  const session = await getServerSession();
+
+  if (session) {
+    redirect('/');
+  }
+
+  return <Form />;
+}
